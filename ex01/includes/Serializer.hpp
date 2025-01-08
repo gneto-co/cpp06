c720cf1307efc571db7c1dbbf0fd716a14ce9e43
gneto-co <gneto-co@student.42.fr>
2025-01-08 11:15:05 +0000

    ex00: done

---
+++ ex01/includes/Serializer.hpp
@@ -0,0 +1,56 @@
+#ifndef SERIALIZER_HPP
+#define SERIALIZER_HPP
+
+#include "Strings.h"
+
+class Serializer
+{
+private:
+	/* constructors */
+	Serializer();
+
+	/* attributes */
+	STRING const _original_value;
+	int _original_type;
+
+	char _char_value;
+	int _int_value;
+	float _float_value;
+	double _double_value;
+	
+	char _char_status;
+	int _int_status;
+	float _float_status;
+	double _double_status;
+
+	/* methods */
+	void from_char();
+	void from_int();
+	void from_float();
+	void from_double();
+	void from_float_nan_inf();
+	void from_double_nan_inf();
+
+	void print_values();
+	void find_value();
+
+	/* getters */
+
+public:
+	/* constructors destructors */
+	Serializer(STRING value);
+	Serializer(const Serializer &copy);
+	~Serializer();
+
+	/* overload operators */
+	Serializer &operator=(const Serializer &src);
+
+	/* exceptions */
+	class InvalidArgumentsException : public std::exception
+	{
+	public:
+		virtual const char *what() const throw();
+	};
+};
+
+#endif
---
+++ ex01/src/main.cpp
@@ -0,0 +1,37 @@
+#include "../includes/Serializer.hpp"
+
+#include <termios.h>
+#include <unistd.h>
+void press_any_key()
+{
+	PRINT << LOW_BRIGHT << "Press any key ( )" << CURSOR_LEFT(2);
+
+	struct termios old_terminal, newt;
+	tcgetattr(STDIN_FILENO, &old_terminal);
+	newt = old_terminal;
+	newt.c_lflag &= ~(ICANON | ECHO);
+	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+	char c = getchar();
+	PRINT << c << CURSOR_RIGHT(1) << RESEND;
+
+	tcsetattr(STDIN_FILENO, TCSANOW, &old_terminal);
+}
+
+int main(int ac, char **av)
+{
+	try
+	{
+		if (ac != 2)
+			throw Serializer::InvalidArgumentsException();
+		else
+		{
+			MAIN_MSG("Serializer")
+			Serializer Serializer(av[1]);
+		}
+	}
+	CATCH
+	SPACER(1)
+	// press_any_key();
+	return 0;
+}
